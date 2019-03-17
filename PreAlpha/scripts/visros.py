d792c6abc76962b942fa5ac8ed97e554e7fe9217
Austin C <akc257@nau.edu>
2019-03-17 16:31:39 -0700

    Now with Camel Case!

--- PreAlpha/scripts/visros.py
+++ PreAlpha/scripts/visros.py
@@ -23,139 +23,139 @@ ZLAB = "Z Label"
 REFRESH_RATE = 10
 REFRESH_WAIT = 1.0/REFRESH_RATE
 
-new_values = []
+newValues = []
 
 class VisualizationData:
-    def __init__(self, obstacles, sensors, configlist):
+    def __init__(self, obstacles, sensors, configList):
         # Class Variables
         self.sensors = sensors
 
         # List of all drones most recent location
-        self.newlocs = []
+        self.newLocs = []
 
         # List of lists of each drones location history
-        self.allx = []
-        self.ally = []
-        self.allz = []
+        self.allX = []
+        self.allY = []
+        self.allZ = []
 
         # Calculate obstacle paths
         # Initialize lists
-        self.obstaclex = []
-        self.obstacley = []
-        self.obstaclez = []
+        self.obstacleX = []
+        self.obstacleY = []
+        self.obstacleZ = []
 
         for sh in obstacles:
             # Loop one face
-            shapex = [sh[0][0],sh[0][0],sh[0][0],sh[0][0],sh[0][0]]
-            shapey = [sh[0][1],sh[0][1],sh[1][1],sh[1][1],sh[0][1]]
-            shapez = [sh[1][2],sh[0][2],sh[0][2],sh[1][2],sh[1][2]]
+            shapeX = [sh[0][0],sh[0][0],sh[0][0],sh[0][0],sh[0][0]]
+            shapeY = [sh[0][1],sh[0][1],sh[1][1],sh[1][1],sh[0][1]]
+            shapeZ = [sh[1][2],sh[0][2],sh[0][2],sh[1][2],sh[1][2]]
 
             # Loop opposite face, at each point jump to matching point on previous face
-            shapex.extend([sh[1][0],sh[1][0],sh[0][0],sh[1][0],sh[1][0],sh[0][0],sh[1][0],sh[1][0] ,sh[0][0],sh[1][0],sh[1][0]])
-            shapey.extend([sh[0][1],sh[1][1],sh[1][1],sh[1][1],sh[1][1],sh[1][1],sh[1][1],sh[0][1] ,sh[0][1],sh[0][1],sh[0][1]])
-            shapez.extend([sh[1][2],sh[1][2],sh[1][2],sh[1][2],sh[0][2],sh[0][2],sh[0][2],sh[0][2] ,sh[0][2],sh[0][2],sh[1][2]])
+            shapeX.extend([sh[1][0],sh[1][0],sh[0][0],sh[1][0],sh[1][0],sh[0][0],sh[1][0],sh[1][0] ,sh[0][0],sh[1][0],sh[1][0]])
+            shapeY.extend([sh[0][1],sh[1][1],sh[1][1],sh[1][1],sh[1][1],sh[1][1],sh[1][1],sh[0][1] ,sh[0][1],sh[0][1],sh[0][1]])
+            shapeZ.extend([sh[1][2],sh[1][2],sh[1][2],sh[1][2],sh[0][2],sh[0][2],sh[0][2],sh[0][2] ,sh[0][2],sh[0][2],sh[1][2]])
 
-            self.obstaclex.append(shapex)
-            self.obstacley.append(shapey)
-            self.obstaclez.append(shapez)
+            self.obstacleX.append(shapeX)
+            self.obstacleY.append(shapeY)
+            self.obstacleZ.append(shapeZ)
 
 
-    def update_locations(self, newlocs):
+    def updateLocations(self, newLocs):
 
-        if(self.newlocs == []):
-            for drone in newlocs:
-                self.allx.append([])
-                self.ally.append([])
-                self.allz.append([])
+        if(self.newLocs == []):
+            for drone in newLocs:
+                self.allX.append([])
+                self.allY.append([])
+                self.allZ.append([])
 
         # Update most recent locations
-        self.newlocs = newlocs
+        self.newLocs = newLocs
 
         # Place points into array for graphing
-        for dronecount, drone in enumerate(newlocs):
-            self.allx[dronecount].append(drone[0])
-            self.ally[dronecount].append(drone[1])
-            self.allz[dronecount].append(drone[2])
+        for droneCount, drone in enumerate(newLocs):
+            self.allX[droneCount].append(drone[0])
+            self.allY[droneCount].append(drone[1])
+            self.allZ[droneCount].append(drone[2])
 
-    def update_plot(self, axis, configlist):
+    def updatePlot(self, axis, configList):
 
         # Plot Drone locations
-        if(configlist[1]):
-            for dronecount, drone in enumerate(self.allx):
-                axis.scatter(self.allx[dronecount][-1], self.ally[dronecount][-1], self.allz[dronecount][-1], c=DRONE_COLOR, marker='v', alpha = 1)
+        if(configList[1]):
+            for droneCount, drone in enumerate(self.allX):
+                axis.scatter(self.allX[droneCount][-1], self.allY[droneCount][-1], self.allZ[droneCount][-1], c=DRONE_COLOR, marker='v', alpha = 1)
 
         # Plot drone paths
-        if(configlist[2]):
-            for dronecount, drone in enumerate(self.allx):
-                axis.plot(self.allx[dronecount], self.ally[dronecount], self.allz[dronecount], c=OPATH_COLOR, linewidth = 0.75)
+        if(configList[2]):
+            for droneCount, drone in enumerate(self.allX):
+                axis.plot(self.allX[droneCount], self.allY[droneCount], self.allZ[droneCount], c=OPATH_COLOR, linewidth = 0.75)
 
         # Plot expected paths
-        if(configlist[3]):
+        if(configList[3]):
             #self.plot_expected_paths(axis)
             pass
 
         # Plot sensors
-        if(configlist[4]):
+        if(configList[4]):
             axis.scatter(self.sensors[0], self.sensors[1], self.sensors[2], c=SENSOR_COLOR, marker='^')
 
         # Plot obstacles
-        if(configlist[5]):
-            for obstaclecount, obstacle in enumerate(self.obstaclex):
-                axis.plot(self.obstaclex[obstaclecount], self.obstacley[obstaclecount], self.obstaclez[obstaclecount], c=SHAPE_COLOR, linewidth = 0.75)
+        if(configList[5]):
+            for obstacleCount, obstacle in enumerate(self.obstacleX):
+                axis.plot(self.obstacleX[obstacleCount], self.obstacleY[obstacleCount], self.obstacleZ[obstacleCount], c=SHAPE_COLOR, linewidth = 0.75)
 
 
 
 """def testing():
-    configlist = [1,0,1,1,1,1]
+    configList = [1,0,1,1,1,1]
     vd = VisualizationData([[[1,4,0],[3,2,2]], [[4,0,5],[0,1,7]]],
             [[-4, -4, 4, 4, -4, -4, 4, 4],
             [4, -4, 4, -4, 4, -4, 4, -4],
-            [0, 0, 0, 0, 8, 8, 8, 8]], configlist)
+            [0, 0, 0, 0, 8, 8, 8, 8]], configList)
     # Set up plotting stuff
     fig = plt.figure()
     fig.canvas.set_window_title('Drone Visualization')
     ax = fig.add_subplot(111, projection='3d')
-    vd.update_locations([[0,0,4], [0,2,0]])
-    vd.update_plot(ax, configlist)
+    vd.updateLocations([[0,0,4], [0,2,0]])
+    vd.updatePlot(ax, configList)
     plt.pause(1)
-    vd.update_locations([[0,0,3], [0,1,0]])
-    vd.update_plot(ax, configlist)
+    vd.updateLocations([[0,0,3], [0,1,0]])
+    vd.updatePlot(ax, configList)
     plt.pause(1)
-    vd.update_locations([[0,0,2], [0,0,0]])
-    vd.update_plot(ax, configlist)
-    # ani = animation.FuncAnimation(fig, vd.update_plot, interval=100)
-    vd.update_plot(ax, configlist)
+    vd.updateLocations([[0,0,2], [0,0,0]])
+    vd.updatePlot(ax, configList)
+    # ani = animation.FuncAnimation(fig, vd.updatePlot, interval=100)
+    vd.updatePlot(ax, configList)
     plt.pause(5)
     plt.show()"""
 
 def callback(data):
-    global new_values
-    new_values = ast.literal_eval(data.data)
+    global newValues
+    newValues = ast.literal_eval(data.data)
 
-def update_vis(vd, axis, configlist):
+def updateVis(vd, axis, configList):
     # Get most recent location and store in local then update
-    global new_values
-    local_new_val = new_values
-    print local_new_val
-    vd.update_locations(local_new_val)
-    vd.update_plot(axis, configlist)
+    global newValues
+    localNewVal = newValues
+    print localNewVal
+    vd.updateLocations(localNewVal)
+    vd.updatePlot(axis, configList)
     plt.pause(REFRESH_WAIT)
 
 def main():
 
-    configlist = [1,0,1,1,1,1]
+    configList = [1,0,1,1,1,1]
 
     """
     vd = VisualizationData([[[1,4,0],[3,2,2]], [[4,0,5],[0,1,7]]],
             [[-4, -4, 4, 4, -4, -4, 4, 4],
             [4, -4, 4, -4, 4, -4, 4, -4],
-            [0, 0, 0, 0, 8, 8, 8, 8]], configlist)
+            [0, 0, 0, 0, 8, 8, 8, 8]], configList)
     """
 
     vd = VisualizationData([[[1,1,4],[4,4,8]]],
             [[-4, -4, 4, 4, -4, -4, 4, 4],
             [4, -4, 4, -4, 4, -4, 4, -4],
-            [0, 0, 0, 0, 8, 8, 8, 8]], configlist)
+            [0, 0, 0, 0, 8, 8, 8, 8]], configList)
 
     # Set up plotting stuff
     fig = plt.figure()
@@ -168,8 +168,8 @@ def main():
 
     rate = rospy.Rate(REFRESH_RATE)
     while (not rospy.is_shutdown()):
-        #Fetch most recent new_values and update plot with them
-        update_vis(vd, ax, configlist)
+        #Fetch most recent newValues and update plot with them
+        updateVis(vd, ax, configList)
 
         rate.sleep()
 
